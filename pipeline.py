7f7369107b1dc49243c0cce190cc9bee891dbefd
noamlr <nlapar@uni.pe>
2021-02-17 16:49:27 -0300

    branch for only segmentation

--- pipeline.py
+++ pipeline.py
@@ -5,14 +5,12 @@ import os
 import pandas as pd
 import json
 import requests
-import pickle
 
 import subprocess
 import shutil
 
 import time
 from variables import *
-from models import *
 
 
 def get_status_csv():
@@ -27,20 +25,6 @@ def get_status_csv():
 
 def save_status_csv(df):
     df.to_csv(BASE_LOG_CSV_PATH, index=False)
-
-
-def check_new_patients(df, base_dir):
-    if not os.path.exists(base_dir):
-        print(f'Path: {base_dir} does not exist')
-        return []
-
-    df_patients = df['name'].to_list()
-    ls_dir = glob.glob(f'{base_dir}/*')
-    dir_patients = [ls.split('/')[-1] for ls in ls_dir]
-    diff = set(dir_patients) - set(df_patients)
-    list_diff = list(diff)
-    
-    return list_diff 
 
 
 def insert_or_update_row(df, patient=None, column=None, value=None):
@@ -161,288 +145,6 @@ def phnn_segmentation(base_nii_dir=None, base_output_dir=None, patient=None, thr
         return (False, f'Error: {e}')
 
 
-def mitk_views_maker(base_nii_dir=None, base_output_dir=None, patient=None, tf_path=None, width=None, height=None, slices=None, axis=None):
-    input_dir = ''
-    output_dir = BASE_MITK_VIEWS_OUTPUT_DIR
-    if base_nii_dir is not None:
-        input_dir = f'{base_nii_dir}/{patient}'
-    else:
-        input_dir = f'{BASE_SEGMENTED_OUTPUT_DIR}/{patient}'
-    if base_output_dir is not None:
-        output_dir = base_output_dir
-    if tf_path is None:
-        tf_path = MITK_TRANSFER_FUNCTION_PATH
-    
-    if not os.path.exists(output_dir):
-        return (False, f'Path {output_dir} does not exist')
-    if not os.path.exists(input_dir):
-        return (False, f'Path {input_dir} does not exist')
-    if not os.path.exists(tf_path):
-        return (False, f'Path {tf_path} does not exist')
-    
-    if width is None:
-        width = MITK_VIEWS_WIDTH
-    if height is None:
-        height = MITK_VIEWS_HEIGHT 
-    if slices is None:
-        slices = MITK_VIEWS_LENGTH 
-    if axis is None:
-        axis = MITK_VIEWS_AXIS
-    
-    #### To get MITK_VIEWS_LENGTH/2 will be odd and MITK_VIEWS_LENGTH > 14, mandatory for script
-    if slices % 2 == 1:
-        slices +=1 
-    if (slices / 2) % 2 == 0:
-        slices += 2
-    slices = max(14, slices)
-
-    try:
-        #### Creating patient views output dirs /output/patient/axisI/
-        output_dir = f'{output_dir}/{patient}'
-        if not os.path.exists(output_dir):
-            os.mkdir(output_dir)
-        for i in range(axis):
-            name_axis = 'axis'+str(i+1)
-            t_path = f'{output_dir}/{name_axis}'
-            if not os.path.exists(t_path):
-                os.mkdir(t_path)
-            else:
-                shutil.rmtree(t_path, ignore_errors=True)
-                os.mkdir(t_path)
-
-        #### Iterate over nifti and build command
-        nii_list = glob.glob(f'{input_dir}/*/crop_by_mask*.nii.gz')
-        print(f'{input_dir}/*/crop_by_mask*.nii.gz')
-        print(nii_list)
-        for ct_image in nii_list:
-            command = f'vglrun {MITK_VIEWS_EXECUTABLE_PATH} -tf {tf_path} -i {ct_image} -o {output_dir} -w {width} -h {height} -c {slices} -a{axis}'
-            # command = f'{MITK_VIEWS_EXECUTABLE_PATH} -tf {tf_path} -i {ct_image} -o {output_dir} -w {width} -h {height} -c {slices} -a{axis}'
-            print(command)
-            mitk_views = run_in_shell(command, is_python=False)
-            print(mitk_views)
-        return (True, '')
-    except Exception as e:
-        return (False, f'Error: {e}')
-
-
-def mitk_video_maker(base_nii_dir=None, base_output_dir=None, patient=None, tf_path=None, width=None, height=None, time=None, fps=None):
-    input_dir = ''
-    output_dir = BASE_MITK_VIDEO_OUTPUT_DIR
-    if base_nii_dir is not None:
-        input_dir = f'{base_nii_dir}/{patient}'
-    else:
-        input_dir = f'{BASE_SEGMENTED_OUTPUT_DIR}/{patient}'
-    if base_output_dir is not None:
-        output_dir = base_output_dir
-    if tf_path is None:
-        tf_path = MITK_TRANSFER_FUNCTION_PATH
-    
-    if not os.path.exists(output_dir):
-        return (False, f'Path {output_dir} does not exist')
-    if not os.path.exists(input_dir):
-        return (False, f'Path {input_dir} does not exist')
-    if not os.path.exists(tf_path):
-        return (False, f'Path {tf_path} does not exist')
-    
-    if width is None:
-        width = MITK_VIDEO_WIDTH
-    if height is None:
-        height = MITK_VIDEO_HEIGHT 
-    if time is None:
-        time = MITK_VIDEO_TIME
-    if fps is None:
-        fps = MITK_VIDEO_FPS
-    
-    try:
-        #### Iterate over nifti and build command
-        nii_list = glob.glob(f'{input_dir}/*/crop_by_mask*.nii.gz')
-        for ct_image in nii_list:
-            command = f'vglrun {MITK_VIDEO_EXECUTABLE_PATH} -tf {tf_path} -i {ct_image} -o {output_dir}/{patient}.mp4 -w {width} -h {height} -t {time} -f {fps}'
-            # command = f'{MITK_VIDEO_EXECUTABLE_PATH} -tf {tf_path} -i {ct_image} -o {output_dir}/{patient}.mp4 -w {width} -h {height} -t {time} -f {fps}'
-            print(command)
-            mitk_views = run_in_shell(command, is_python=False)
-            print(mitk_views)
-        return (True, f'{output_dir}/{patient}.mp4')
-    except Exception as e:
-        return (False, f'Error: {e}')
-
-
-def process_prediction(base_model_dir=None, base_legend_dir=None, base_slices_dir=None, patient=None, width=None, height=None, axis=None, classes=None):
-    if base_slices_dir is None:
-        base_slices_dir = BASE_MITK_VIEWS_OUTPUT_DIR
-    if base_model_dir is None:
-        base_model_dir = PREDICTION_MODEL_PATH
-    if base_legend_dir is None:
-        base_legend_dir = PREDICTION_LEGEND_PATH
-    
-    if not os.path.exists(base_slices_dir):
-        return (False, {'error': f'Path {base_slices_dir} does not exist'})
-    if not os.path.exists(base_model_dir):
-        return (False, {'error': f'Path {base_model_dir} does not exist'})
-    if not os.path.exists(base_legend_dir):
-        return (False, {'error': f'Path {base_legend_dir} does not exist'})
-    
-    if width is None:
-        width = PREDICTION_WIDTH
-    if height is None:
-        height = PREDICTION_HEIGHT 
-    if axis is None:
-        axis = PREDICTION_AXIS
-    if classes is None:
-        classes = PREDICTION_CLASSES
-    
-    try:
-        #### Iterate over nifti and build command
-        model_name = 'resnet101'
-        command = f'{PYTHON_PATH} models.py --md {base_model_dir} --ld {base_legend_dir} --sd {base_slices_dir} --mn {model_name} --w {width} --h {height} --a {axis} --p {patient}'
-        print(command)
-        pred = run_in_shell(command, is_python=True)
-        print(pred)
-        result = None
-        with open('prediction_result.pkl', 'rb') as pr:
-            result = pickle.load(pr)
-        # model_cidia = ModelCidia(base_model_dir, base_legend_dir, base_slices_dir, model_name, width, height)
-        # bool_status, result = model_cidia.test_patient(axis, patient)
-
-        if result is None or result['success'] is False:
-            return (False, result)
-        return (True, result)
-    except Exception as e:
-        return (False, {'error': f'Error: {e}'})
-
-
-def upload_video(study_id=None, video_path=None):
-    if study_id is None:
-        return (False, {'error': f'study_id is NULL'})
-    if study_id is None:
-        return (False, {'error': f'video_path is NULL'})
-    
-    if not os.path.exists(video_path):
-        return (False, {'error': f'Path {video_path} does not exist'})
-    try:
-        video_name = video_path.split('/')[-1] 
-        #### Iterate over study_id and build command
-        for study in study_id:
-            command = f'curl --request PUT --data-binary "@{video_path}" https://cidia.ufrgs.dev/storage/v1/exam-media/{study}/3d-video/{video_name}'
-            video = run_in_shell(command, is_python=False)
-            # print(command)
-            print(video)
-        return (True, '')
-    except Exception as e:
-        return (False, f'Error: {e}')
- 
-def upload_results(url=None, study_id=None, data=None):
-    if url is None:
-        url = API_URL
-    
-    if study_id is None:
-        return (False, {'error': f'study_id is NULL'})
-    if study_id is None:
-        return (False, {'error': f'data is NULL'})
-    
-    try:
-        #### Iterate over study_id and build command
-        output = 1
-        if data['percentage'] < 50.0:
-            output = 2
-        for study in study_id:
-            payload = {'series_id': None, 'study_id': study, 'algorithm_id': 'metadata', 'output': output, 
-                    'metadata': {'name':data['name'], 'radiology': '', 'volume_rendering': data['percentage']}}
-            r = requests.post(url, data=json.dumps(payload))
-            # print(payload)
-            print(r)
-        return (True, '')
-    except Exception as e:
-        return (False, f'Error: {e}')
-
-
-
-def run_pipeline(patient_list=None):
-    df = get_status_csv()
-    # list_patients = check_new_patients(df, BASE_DICOM_INPUT_DIR)
-    result = {'success': True, 'detail': ''}
-    for patient in patient_list:
-        print(patient)
-        print('Init time: ' + time.strftime("%Y-%m-%d %H:%M:%S"))
-        # CALL DICOM -> NIFTI
-        a, b = None, None #dicom_path, nii_path
-        if len(patient) < 5: is_hmv = True
-        else: is_hmv = False
-        
-        study_id = None
-        
-        bool_result, text_out = convert_dicom_to_nifti(base_input_dir=a, base_output_dir=b, patient=patient, is_hmv=is_hmv)
-        if bool_result:
-            df = insert_or_update_row(df, patient=patient, column='to_nifti', value=True)
-            df = insert_or_update_row(df, patient=patient, column='study_id', value=text_out)
-            study_id = text_out
-        else:
-            result['success'] = False
-            result['detail'] += f'Dicom to nifti: {text_out}'
-        print('Convert dicom -> nifti: ' + time.strftime("%Y-%m-%d %H:%M:%S"))
-        
-        # CALL PHNN SEGMENTATION
-        c = None # nii_segmented_path
-        bool_result, text_out = phnn_segmentation(base_nii_dir=b, base_output_dir=c, patient=patient, threshold=None, batch_size=None)
-        if bool_result:
-            df = insert_or_update_row(df, patient=patient, column='segmented', value=True)
-        else:
-            result['success'] = False
-            result['detail'] += f'\nPhnn segmentation: {text_out}'
-        print('Segment nifti: ' + time.strftime("%Y-%m-%d %H:%M:%S"))
-
-        # CALL MITK VIEWS
-        d, e = None, None # slices_path, tf_file_path
-        
-        bool_result, text_out = mitk_views_maker(base_nii_dir=c, base_output_dir=d, patient=patient, tf_path=e, width=None, height=None, slices=None, axis=None)
-        if bool_result:
-            df = insert_or_update_row(df, patient=patient, column='to_slices_3d', value=True)
-        else:
-            result['success'] = False
-            result['detail'] += f'\nSlices 2d: {text_out}'
-        print('slices 2d: ' + time.strftime("%Y-%m-%d %H:%M:%S"))
-        
-        # CALL MITK VIDEOS
-        f = None # videos_path
-        bool_result, text_out = mitk_video_maker(base_nii_dir=c, base_output_dir=f, patient=patient, tf_path=e, width=None, height=None, time=None, fps=None)
-        if bool_result:
-            df = insert_or_update_row(df, patient=patient, column='to_video', value=True)
-        else:
-            result['success'] = False
-            result['detail'] += f'\nVideo: {text_out}'
-        print('Videos: ' + time.strftime("%Y-%m-%d %H:%M:%S"))
-        
-        bool_result, text_out = upload_video(study_id=study_id, video_path=text_out)
-        if not bool_result:
-            result['success'] = False
-            result['detail'] += f'\nUpload video: {text_out}'
-        
-        # CALL PROCESS PREDICT
-        g, h = None, None
-        w, h, ax, cl = None, None, None, None
-        bool_result, text_out = process_prediction(base_model_dir=g, base_legend_dir=h, base_slices_dir=d, patient=patient, width=w, height=h, axis=ax, classes=cl)
-        if bool_result:
-            df = insert_or_update_row(df, patient=patient, column='predicted', value=text_out['predicted'])
-            df = insert_or_update_row(df, patient=patient, column='percentage', value=text_out['percentage'])
-            df = insert_or_update_row(df, patient=patient, column='axis_detail', value=text_out['axis_detail'])
-            df = insert_or_update_row(df, patient=patient, column='axis_qty', value=text_out['axis_qty'])
-            print(df)
-            
-            u = None
-            text_out['name'] = patient
-            bool_result, text_out = upload_results(url=u, study_id=study_id, data=text_out)
-            if not bool_result:
-                result['success'] = False
-                result['detail'] += f'\nUpload result: {text_out}'
-            else: #Success -> save dataframe
-                save_status_csv(df)
-        else:
-            result['success'] = False
-            result['detail'] += f'\nPrediction process: {text_out}'
-        print('Prediction: ' + time.strftime("%Y-%m-%d %H:%M:%S"))
-        
-    return result
-
 def convert_and_segment(patient_list=None):
     df = get_status_csv()
     # list_patients = check_new_patients(df, BASE_DICOM_INPUT_DIR)
@@ -487,35 +189,23 @@ def convert_and_segment(patient_list=None):
 
     return result
 
+if __name__ == '__main__':
+    import argparse
+    import os
+    
+    try:
+        parser = argparse.ArgumentParser(description='Convert to nii and segment with P-HNN.')
 
+        parser.add_argument('--patient', type=str, required=True, help='patient name, sample --patient TYP-028')
 
-###################################################
-## Flask service
-###################################################
-#!flask/bin/python
-from flask import Flask
-from flask import request
-import json
-import sys
-
-app = Flask(__name__)
-
-@app.route('/pipeline3/<ct_path>', methods=['GET'])
-def index(ct_path):
-    if ct_path is not None:
-        results = run_pipeline(patient_list=[ct_path])
-        out = json.dumps(results)
-    return out
-
-
-@app.route('/segment/<ct_path>', methods=['GET'])
-def segment(ct_path):
-    if ct_path is not None:
-        results = convert_and_segment(patient_list=[ct_path])
-        out = json.dumps(results)
-    return out
+        args = parser.parse_args()
+        if args.patient is not None:
+            results = convert_and_segment(patient_list=[args.patient])
+        else:
+            print("Patient could not be NULL")
+    except Exception as e:
+         print(f'Error: {e}')
 
 
 
-# if __name__ == '__main__':
 #     app.run(debug=True)
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from pipeline import app
-
-if __name__ == "__main__":
-    app.run()
